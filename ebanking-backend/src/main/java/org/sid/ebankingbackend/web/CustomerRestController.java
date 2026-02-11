76c79d69ed398e08f75fa7facec9bf93d3160076
hamzaatlili <162675281+hamzaatlili@users.noreply.github.com>
2026-02-11 12:23:08 +0100

    Update CustomerRestController.java

--- ebanking-backend/src/main/java/org/sid/ebankingbackend/web/CustomerRestController.java
+++ ebanking-backend/src/main/java/org/sid/ebankingbackend/web/CustomerRestController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @Slf4j
 //J'autorise toute les domaine
-/*@CrossOrigin(origins = "http://192.168.1.54:4200")
+/*@CrossOrigin(origins = "http://192.168.1.54:8085")
 public class CustomerRestController {
     private BankAccountService bankAccountService;
 */
